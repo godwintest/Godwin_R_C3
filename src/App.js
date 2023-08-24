ac7d52227d2be36ded60c111d81dd0d18fe82c5e
godwintest <etornamkofi5@gmail.com>
2023-08-24 21:20:21 +0100

    Updated App.js page with more details

--- src/App.js
+++ src/App.js
@@ -4,8 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h1>Hello world</h1>
-    <p>I am Edor Godwin Etornam, This is my first React application</p>
+    <h1>My Portfolio Page</h1>
+    <p>I am Edor Godwin Etornam. I am a passionate Frontend Developer with 3 years experience in mobile responsive website and web application developement. I hold BSc. Degree in Telecommunication Engineering.</p>
+
+    <h2>Skills</h2>
+    <ol>
+    <li>Html</li>
+    <li>CSS</li>
+    <li>JavaScript</li>
+    <li>Reat</li>
+    </ol>
+    <h2>Education</h2>
+    <ol>
+    <li>BSc Telecommunication Engineering - Knust</li>
+    </ol>
+    
     </div>
   );
 }
